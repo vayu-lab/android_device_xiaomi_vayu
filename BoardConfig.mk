9b84bb7ea407e74cf190c13f6d7117e33e8c1eb9
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2019-10-21 22:29:07 +0200

    vayu: Add assert for vayu and bhima

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,4 +47,7 @@ TARGET_SYSTEM_EXT_PROP := $(DEVICE_PATH)/configs/properties/system_ext.prop
 TARGET_SYSTEM_PROP := $(DEVICE_PATH)/configs/properties/system.prop
 TARGET_VENDOR_PROP := $(DEVICE_PATH)/configs/properties/vendor.prop
 
+#  Assert
+TARGET_OTA_ASSERT_DEVICE := vayu,bhima
+
 -include vendor/xiaomi/vayu/BoardConfigVendor.mk
