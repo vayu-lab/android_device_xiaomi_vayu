ba486cc6fbdf631526b14ce0db2f974a99d3f73e
Rashed Abdel-Tawab <rashed@linux.com>
2021-03-14 13:18:16 +0100

    vayu: Import extract utils
    * Create empty proprietary files list
    
    Change-Id: I9d3bb2c8757ea346291271902ba2a034b5f7ece9

--- extract-files.sh
+++ extract-files.sh
@@ -3,7 +3,17 @@
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2021 The LineageOS Project
 #
-# SPDX-License-Identifier: Apache-2.0
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 set -e
@@ -15,9 +25,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ANDROID_ROOT="${MY_DIR}/../../.."
+ARROW_ROOT="${MY_DIR}"/../../..
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${ARROW_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -42,6 +52,9 @@ while [ "${#}" -gt 0 ]; do
                 SECTION="${2}"; shift
                 CLEAN_VENDOR=false
                 ;;
+        -f | --force )
+                FORCE=true
+                ;;
         * )
                 SRC="${1}"
                 ;;
@@ -55,8 +68,7 @@ fi
 
 # Initialize the helper
 setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" true "${CLEAN_VENDOR}"
-
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
-        "${KANG}" --section "${SECTION}"
-
-"${MY_DIR}/setup-makefiles.sh"
+function blob_fixup() {
+    case "${1}" in
+    esac
+}
