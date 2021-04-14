5db3c7b3056d39fdf39502b133e8de9694caef2a
kubersharma001 <kubersharma001@gmail.com>
2021-04-14 14:22:02 +0000

    vayu: Sync scripts with template
    Change-Id: Ia7ed4e1ac6609617360c6c503591dc60328e8eae

--- extract-files.sh
+++ extract-files.sh
@@ -3,17 +3,7 @@
 # Copyright (C) 2016 The CyanogenMod Project
 # Copyright (C) 2017-2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
 #
 
 set -e
@@ -25,9 +15,9 @@ VENDOR=xiaomi
 MY_DIR="${BASH_SOURCE%/*}"
 if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
-ARROW_ROOT="${MY_DIR}"/../../..
+ANDROID_ROOT="${MY_DIR}/../../.."
 
-HELPER="${ARROW_ROOT}/tools/extract-utils/extract_utils.sh"
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -63,9 +53,6 @@ while [ "${#}" -gt 0 ]; do
                 SECTION="${2}"; shift
                 CLEAN_VENDOR=false
                 ;;
-        -f | --force )
-                FORCE=true
-                ;;
         * )
                 SRC="${1}"
                 ;;
@@ -83,3 +70,8 @@ function blob_fixup() {
     case "${1}" in
     esac
 }
+
+extract "${MY_DIR}/proprietary-files.txt" "${SRC}" \
+        "${KANG}" --section "${SECTION}"
+
+"${MY_DIR}/setup-makefiles.sh"
