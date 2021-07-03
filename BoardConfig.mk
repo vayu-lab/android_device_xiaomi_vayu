d0b35bed0bcef30426d71da9596200655d21e2f8
yashlearnpython <73551736+yash3056@users.noreply.github.com>
2021-07-03 12:34:27 +0530

    vayu: Add Architecture rule

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,21 @@ BOARD_VENDOR := xiaomi
 # Security patch level
 VENDOR_SECURITY_PATCH := 2021-03-01
 
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a76
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv8-a
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
+TARGET_2ND_CPU_VARIANT_RUNTIME := cortex-a76
+
 # HIDL
 DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/vintf/manifest.xml
 DEVICE_MATRIX_FILE := $(DEVICE_PATH)/vintf/compatibility_matrix.xml
